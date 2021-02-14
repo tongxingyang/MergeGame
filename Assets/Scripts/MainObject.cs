2092cec9d6f8c9c72a58795661de0773e17dc86f
moongeonsu <ssunine21@gamil.com>
2021-02-14 23:29:30 +0900

    Initial commit

---
+++ Assets/Scripts/MainObject.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class MainObject : MonoBehaviour {
+	string TAG = "mainObject";
+
+	public enum Type {
+		s,
+		m,
+		l,
+		xl
+	}
+
+	public Type type;
+	private Rigidbody2D rigidbody;
+
+	private void Start() {
+		rigidbody = this.GetComponent<Rigidbody2D>();
+	}
+
+	private void OnCollisionEnter2D(Collision2D collision) { 
+		if (collision.gameObject.CompareTag(TAG)) {
+			if (collision.gameObject.GetComponent<MainObject>().type == this.type) {
+
+				if (rigidbody.velocity.x > collision.gameObject.GetComponent<Rigidbody2D>().velocity.x || rigidbody.velocity.y > collision.gameObject.GetComponent<Rigidbody2D>().velocity.y) {
+					this.gameObject.SetActive(false);
+					return;
+				} else {
+					Debug.Log(rigidbody.velocity);
+					if (this.type == Type.m) return;
+					this.spawn();
+					this.gameObject.SetActive(false);
+				}
+			}
+		}
+	}
+
+	public void spawn() {
+		SpwanManager.init.objectSpawn((int)this.type + 1, this.transform.position);
+	}
+}
---
+++ Assets/Scripts/SpwanManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class SpwanManager : MonoBehaviour {
+	private float INIT_Y_POSITION = 4;
+
+	public static SpwanManager init = null;
+	private void Awake() {
+		if(init == null) {
+			init = this;
+		} else if (init != this) {
+			Destroy(this.gameObject);
+		}
+		DontDestroyOnLoad(this.gameObject);
+	}
+
+	public GameObject[] mainObjects;
+	private GameObject currObject;
+
+	private void Start() {
+		objectSpawn();
+	}
+
+	public void objectSpawn() {
+		int rand = Random.Range(0, mainObjects.Length);
+		currObject = Instantiate(mainObjects[rand], new Vector3(0, INIT_Y_POSITION, 0), Quaternion.identity);
+	}
+
+	public void objectSpawn(int type, Vector3 position) {
+		currObject = Instantiate(mainObjects[type], position, Quaternion.identity);
+	}
+
+	private void OnMouseDrag() {
+		Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		currObject.transform.position = new Vector3(mousePosition.x, INIT_Y_POSITION, 0);
+	}
+
+	private void OnMouseUp() {
+		currObject.GetComponent<Rigidbody2D>().gravityScale = 1;
+		objectSpawn();
+	}
+}
